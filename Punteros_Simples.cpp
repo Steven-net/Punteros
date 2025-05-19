289da2131c412acb9d7729ce74db0d9e66d2c8ac
Steven-net <stevenalex0806@gmail.com>
2025-05-18 22:23:31 -0500

    Agregue comentarios

--- Punteros_Simples.cpp
+++ Punteros_Simples.cpp
@@ -1,3 +1,8 @@
+/* En este caso vamor a poner el practica dos tipos de punteros
+  *x --> Este accede a la direccion de memoria y recupera el valor almacenado en ella (desreferencia la direccion de x)
+  &x --> Este nos da la direccion en donde se almacena la variable x
+*/
+
 #include <iostream>
 using namespace std;
 
@@ -6,11 +11,11 @@ int main(){
     int* ptr = &i;
     int** ptrptr = &ptr;
 
-    cout << "ptr: "<<ptr<<endl; // nos da la direccion en donde se almacena i
+    cout << "ptr: "<<ptr<<endl; // nos da la direccion en donde se almacena i (*(ptr) == *(&i) == i)
     cout << "*ptr: "<<*ptr<<endl; // nos da el valor a donde apunta el puntero (en este caso a i=3)
-    cout << "ptrptr: "<<ptrptr<<endl; // nos da la direccion 
-    cout << "*ptrptr: "<<*ptrptr<<endl;
-    cout << "**ptrptr: "<<**ptrptr<<endl;
+    cout << "ptrptr: "<<ptrptr<<endl; // nos da la direccion del puntero que apunta a i 
+    cout << "*ptrptr: "<<*ptrptr<<endl; // nos da la direccion donde se almacena i (*(ptrptr) == *(&ptr) == &i)
+    cout << "**ptrptr: "<<**ptrptr<<endl; // nos da el valor de i (*(*(ptrptr)) == *(&i) == i)
 
     return 0;
 }
